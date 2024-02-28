0cb08f71b23a2f59e848894ac72fd48000be31f7
Bogdan Kozak <bkozak04@gmail.com>
2024-02-28 14:24:03 +0200

    Added http-server

--- server.go
+++ server.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	fmt.Println("Сервер запущено")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
+}
