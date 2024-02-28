a6b596b1086b1c4e53269c6344f4fcb6866fa6a5
Bogdan Kozak <121981921+bifynok@users.noreply.github.com>
2024-02-28 19:06:11 +0200

    Fixed port

--- server.go
+++ server.go
@@ -33,7 +33,7 @@ func main() {
 
 	fmt.Println("Сервер запущено")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8795", nil); err != nil {
 		fmt.Println(err)
 	}
 }
